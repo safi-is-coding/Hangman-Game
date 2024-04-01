1151e35c670a6f202962bccf140ffe5ee6bb0424
Mohammad Safi Maz <142226851+safi-is-coding@users.noreply.github.com>
2024-04-01 23:03:56 +0530

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,105 +1,93 @@
-// word list for the hangman game
-let word_list = [
-    'apple', 'banana', 'cherry', 'date', 'mango', 'kiwi', 'grape', 'orange', 'pineapple', 'watermelon', 'strawberry'
-    ]
 
-let x = Math.floor((Math.random() * word_list.length-1) + 1);
+// word list for the hangman game
+let word_list = ['apple', 'banana', 'orange', 'grape', 'kiwi', 'strawberry', 'blueberry', 'watermelon', 'melon', 'pear', 'peach', 'plum', 'cherry', 'pineapple', 'mango', 'lemon', 'lime', 'grapefruit', 'coconut', 'papaya']
+
+let x = Math.floor((Math.random() * word_list.length - 1) + 1);
 
 let lives = 0
-let chose_word = word_list[x]
-let guess = document.getElementById('guess_letter')
+chose_word = word_list[ x ]
 // console.log(chose_word)
 
 let display = []
-for(i=0;i<chose_word.length;i++){
+for (i = 0; i < chose_word.length; i++) {
     display.push('_');
 }
-document.getElementById("word_length").innerHTML = `The word contains `+ String(display.length) +` letters.`; 
-document.getElementById( 'display' ).innerText = display.join(' ')
+
+document.getElementById("word_length").innerHTML = `The word 
+contains ` + String(display.length) + ` letters.`;
+document.getElementById('display').innerText = display.join(' ')
 
 
 let display2 = chose_word.split('')
 
-let input = document.getElementById("guess_letter");
-input.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    document.getElementById("submit-btn").click();
-  }
-});
+// let input = document.getElementById("guess_letter");
+// input.addEventListener("keypress", function(event) {
+//   if (event.key === "Enter") {
+//     event.preventDefault();
+//     document.getElementById("submit-btn").click();
+//   }
+// });
 
-function reset(){
+function reset () {
     location.reload()
 }
 
-function check(){
-    // console.log(guess.value.toLowerCase());
+function check (alp) {
     // console.log(chose_word);
 
-    if(guess.value.toLowerCase() == ''){
-        document.getElementById( 'warning' ).innerText = "Please enter a letter."
-    }
-    else
-    {
-        document.getElementById( 'warning' ).innerText = `You've ${10-lives} lives !`
+    document.getElementById('warning').innerText = `You've ${10 - lives} lives !`
 
-        for(i=0;i<display.length;i++){
-            if(guess.value.toLowerCase() == display[i]){
-                document.getElementById( 'warning' ).innerText = `You've already guessed '${guess.value.toLowerCase()}'`
-                // console.log("Already present")
-            }
-        }
-
-        for(i=0;i<chose_word.length;i++) 
-        {
-            letter = guess.value.toLowerCase()
-            if(chose_word[i] == letter){
-                display[i] = letter
-                document.getElementById( 'display' ).innerText = display.join(' ')
-            }
-        }
-    
-        let present=false 
-        for(i=0;i<display2.length;i++)
-        {
-            if(guess.value.toLowerCase() == display2[i])
-            {
-                present=true
-            }
-        }
-        if(!present){
-            // console.log("Not Presnet");
-            lives += 1
-            // console.log('lives ' + lives)
-            if(lives==0){
-                document.getElementById('hangman-image').src=`images/0.jpg`
-            }
-            else if (lives<=10)
-            {
-                document.getElementById( 'warning' ).innerText = `You guessed wrong, Try again you have ${10-lives} left`
-                document.getElementById('hangman-image').src=`images/${lives}.jpg`
-            }
-            else
-            {
-                document.getElementById( 'warning' ).innerText = `The Word was : ${chose_word}`
-                document.getElementById('hangman-image').src=`images/gameover.png`
-
-                document.getElementById("guess_letter").disabled = true
-                document.getElementById("submit-btn").disabled = true
-                document.getElementById("reset").style.display = 'block'
-
-            }
-        }
-        
-        
-        if (display.indexOf('_')== -1 )
-        {
-            document.getElementById( 'warning' ).innerText = `Congratulations! You've guessed the word: ${chose_word}`
-            document.getElementById('hangman-image').src=`images/won.png`
+    for (i = 0; i < display.length; i++) {
+        if (alp == display[ i ]) {
+            document.getElementById('warning').innerText = `You've already guessed '${alp}'`
+            // console.log("Already present")
         }
     }
-    guess.value = ""
+
+    for (i = 0; i < chose_word.length; i++) {
+        letter = alp
+        if (chose_word[ i ] == letter) {
+            display[ i ] = letter
+            document.getElementById('display').innerText = display.join(' ')
+        }
+    }
+
+    let present = false
+    for (i = 0; i < display2.length; i++) {
+        if (alp == display2[ i ]) {
+            present = true
+        }
+    }
+    if (!present) {
+        // console.log("Not Presnet");
+        lives += 1
+        // console.log('lives ' + lives)
+        if (lives == 0) {
+            document.getElementById('hangman-image').src = `images/0.jpg`
+        }
+        else if (lives < 10) {
+            document.getElementById('warning').innerText = `You guessed wrong, Try again you have ${11 - lives} left`
+            document.getElementById('hangman-image').src = `images/${lives}.jpg`
+        }
+        else if (lives == 10) {
+            document.getElementById('warning').innerText = `Try last time as a Bonus from the programmer.`
+            document.getElementById('hangman-image').src = `images/${lives}.jpg`
+        }
+        else {
+            document.getElementById("reset").style.display = 'block'
+            document.getElementById('warning').innerText = `The Word was : ${chose_word}`
+            document.getElementById('hangman-image').src = `images/gameover.png`
+
+        }
+    }
+
+
+    if (display.indexOf('_') == -1) {
+        document.getElementById('warning').innerText = `Congratulations! You've guessed the word: ${chose_word}`
+        document.getElementById('hangman-image').src = `images/won.png`
+        document.getElementById("reset").style.display = 'block'
+    }
 }
 
-    
+
 
