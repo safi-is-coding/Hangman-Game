2d43b1c5011b09e16ae3f770639f80d39a7c3e5b
Mohammad Safi Maz <142226851+safi-is-coding@users.noreply.github.com>
2024-04-04 16:00:15 +0530

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -26,18 +26,14 @@ let x = Math.floor(Math.random() * word_list.length - 1 + 1);
 
 let lives = 0;
 chose_word = word_list[ x ];
-// console.log(chose_word)
+console.log(chose_word)
 
 let display = [];
 for (i = 0; i < chose_word.length; i++) {
     display.push("_");
 }
 
-document.getElementById("word_length").innerHTML =
-    `The word 
-contains ` +
-    String(display.length) +
-    ` letters.`;
+document.getElementById("word_length").innerHTML =`The word contains ` + String(display.length) + ` letters.`;
 document.getElementById("display").innerText = display.join(" ");
 
 let display2 = chose_word.split("");
@@ -54,61 +50,48 @@ function reset () {
     location.reload();
 }
 
-function check (alp) {
-    // console.log(chose_word);
 
-    if (display.indexOf("_") == -1) {
-        disableButtons()
-        document.getElementById("warning").innerText = `Congratulations! You've guessed the word: ${chose_word}`;
-        document.getElementById("hangman-image").src = `images/won.png`;
-        document.getElementById("reset").style.display = "block";
-    }
-
+function check(alp) {
     document.getElementById("warning").innerText = `You've ${10 - lives} lives !`;
 
-    for (i = 0; i < display.length; i++) {
-        if (alp == display[ i ]) {
-            document.getElementById(
-                "warning"
-            ).innerText = `You've already guessed '${alp}'`;
-            // console.log("Already present")
+    if (display.includes('_')) {
+        for (let i = 0; i < display.length; i++) {
+            if (alp === display[i]) {
+                document.getElementById("warning").innerText = `You've already guessed '${alp}'`;
+                return; // No need to continue checking if the letter is already guessed
+            }
         }
-    }
 
-    for (i = 0; i < chose_word.length; i++) {
-        letter = alp;
-        if (chose_word[ i ] == letter) {
-            display[ i ] = letter;
-            document.getElementById("display").innerText = display.join(" ");
+        let letterGuessed = false;
+        for (let i = 0; i < chose_word.length; i++) {
+            if (chose_word[i] === alp) {
+                display[i] = alp;
+                document.getElementById("display").innerText = display.join(" ");
+                letterGuessed = true;
+            }
         }
-    }
 
-    let present = false;
-    for (i = 0; i < display2.length; i++) {
-        if (alp == display2[ i ]) {
-            present = true;
+        if (!letterGuessed) {
+            lives++;
+            if (lives < 10) {
+                document.getElementById("warning").innerText = `You guessed wrong, Try again you have ${10 - lives} left`;
+                document.getElementById("hangman-image").src = `images/${lives}.jpg`;
+            } else {
+                document.getElementById("reset").style.display = "block";
+                document.getElementById("warning").innerText = `The Word was : ${chose_word}`;
+                document.getElementById("hangman-image").src = `images/over.gif`;
+                disableButtons();
+            }
         }
-    }
-    if (!present) {
-        // console.log("Not Presnet");
-        lives += 1;
-        // console.log('lives ' + lives);
-        if (lives == 0) {
-            document.getElementById("hangman-image").src = `images/0.jpg`;
-        } else if (lives < 10) {
-            document.getElementById("warning").innerText = `You guessed wrong, Try again you have ${11 - lives} left`;
-            document.getElementById("hangman-image").src = `images/${lives}.jpg`;
-        } else if (lives == 10) {
-            document.getElementById("warning").innerText = `Try last time as a Bonus from the programmer.`;
-            document.getElementById("hangman-image").src = `images/${lives}.jpg`;
-        } else {
-            document.getElementById("reset").style.display = "block";
-            document.getElementById("warning").innerText = `The Word was : ${chose_word}`;
-            document.getElementById("hangman-image").src = `images/gameover.png`;
-            disableButtons(); // Call a function to disable buttons
-        }
+    } else {
+        // No underscores left in the display array, meaning all letters have been guessed
+        document.getElementById("hangman-image").src = `images/dance1.gif`;
+        document.getElementById("warning").innerText = `Congratulations! You've guessed the word: ${chose_word}`;
+        document.getElementById("reset").style.display = "block";
+        disableButtons();
     }
 }
+
 
 function disableButtons() {
     let buttons = document.getElementsByClassName('alp-btn');
